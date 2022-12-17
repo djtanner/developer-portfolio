3d9a1385cb98c425d1b0e64b6e0070b59bcf258b
djtanner <zshariat08@gmail.com>
2022-12-17 11:05:23 +0400

    adjusted top padding, added a new greeting class

--- containers/Greetings.jsx
+++ containers/Greetings.jsx
@@ -14,7 +14,7 @@ const Greetings = () => {
 	return (
 		<main>
 			<div className="position-relative">
-				<section className="section section-lg section-shaped pb-250">
+				<section className="section section-greeting section-shaped pb-100">
 					<div className="shape shape-style-1 bg-gradient-info">
 						<span />
 						<span />
