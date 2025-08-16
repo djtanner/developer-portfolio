7c8f145579b03119b540ca45b322ec8fbaf90916
Zhila Shariat <zshariat08@gmail.com>
2025-08-16 11:14:17 -0400

    ed section

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,8 @@ export default function Home({ githubProfileData }) {
       <Greetings />
       <Skills />
       <Projects />
-     {/* <Experience /> */}
-      <Education />
+      {/* <Experience /> */}
+      {/*<Education /> */}
       {showContactUs ? <Contact /> : null}
       <GithubProfileCard prof={githubProfileData} />
     </div>
