4ecd68bd6fcbdcd7b64a7e613a5e02ccbc3aa643
Zhila Shariat <zshariat08@gmail.com>
2025-08-16 11:25:14 -0400

    lottie deployment fix

--- components/DisplayLottie.jsx
+++ components/DisplayLottie.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import Lottie from "react-lottie";
+import dynamic from "next/dynamic";
+
+// Dynamically import Lottie with SSR disabled
+const Lottie = dynamic(() => import("react-lottie"), { 
+	ssr: false,
+	loading: () => <div style={{ height: "200px" }}>Loading...</div>
+});
 
 const GreetingLottie = ({ animationPath }) => {
 	const defaultOptions = {
